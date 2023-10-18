6f038423a3bb52250eacff3b0938fd849441ba90
Luiz Otávio <122511773+Luiz1614@users.noreply.github.com>
2023-10-18 12:03:33 -0300

    terminando o exercicio 7

--- lista-funções/ex7.py
+++ lista-funções/ex7.py
@@ -4,3 +4,14 @@ deverá escrever as seguintes funções:
 - ler_numero(): Solicita um número inteiro e retorna esse número para o programa principal.
 - tabuada(n): Recebe como parâmetro um número inteiro e exibe na tela a tabuada desse número.
 """
+
+def ler_numero():
+  numero = int(Input("Digite um número inteiro: "))
+  return numero
+
+def tabuada():
+  for i in range(1,11)
+    print(f"{n} + {i} = {n * i}")
+
+n = ler_numero()
+tabuada(n)

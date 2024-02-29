27fd870c1f511664cb4e37cb32054513f87def82
Luiz Otávio <luizotavio.ok@gmail.com>
2024-02-29 10:12:05 -0300

    Aula Nextjs

---
+++ FIAP/WEB DEVELOPMENT/my-first-app/app/contato/page.tsx
@@ -0,0 +1,8 @@
+const contato = () => {
+  return (
+    <div>
+      <br></br><h1>Contato</h1>
+    </div>
+  );
+} 
+export default contato;
---
+++ FIAP/WEB DEVELOPMENT/my-first-app/app/fiap/page.tsx
@@ -0,0 +1,8 @@
+export default function FiapPage() {
+  return(
+    <main>
+        <br></br>
+      <h1>Minha Página</h1>
+    </main>
+  );
+}   
